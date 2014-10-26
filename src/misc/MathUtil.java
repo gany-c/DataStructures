32b6656c9b1471b6d2cbdc3b19695da6d4978450
Gany Chidambaram <ganapathy.c@gmail.com>
2014-10-26 13:35:31 -0700

    Logarithmic time exponential function

--- src/misc/MathUtil.java
+++ src/misc/MathUtil.java
@@ -7,7 +7,7 @@ public class MathUtil {
 	    * Returns a^b, as the standard mathematical exponentiation function
 	    */
 	   
-	   public double pow(double a, int b) {
+	   public static double pow(double a, int b) {
 
 		    double output = 1d;
 		    
@@ -31,5 +31,51 @@ public class MathUtil {
 		    return output;
 		        
 		 }
+	   
+	   public static double powWow(double a, int b){
+		   
+		   double output = 1d;
+		   
+		   if(b== 0)
+		   {
+			   return output;
+		   }
+		   else if(b == 1)
+			   return a;
+		   else if(b <0)
+		   {
+			   return(1d/powWow(a,(-1)*b ));
+		   }
+		   else{
+			   
+			   if(b%2 == 0){
+				   double root = powWow(a,b/2);
+				   return root * root;
+			   }
+			   else
+			   {
+				   double root = powWow(a,b/2);
+				   return root * root * a;
+			   }
+			   
+		   }
+	   }
+	   
+	   
+	   public static void main(String[] args){
+		   
+		   System.out.println("Start");
+		   
+		   System.out.println(pow(2,9));
+		   System.out.println(powWow(2,9));
+		   
+		   
+		   System.out.println(pow(2,-3));
+		   System.out.println(powWow(2,-3));
+		   
+		   System.out.println(pow(9,3));
+		   System.out.println(powWow(9,3));
+		   
+	   }
 
 }
