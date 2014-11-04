0d5ceb81bb58d9d1bab94b8be39d896db558586b
Gany Chidambaram <ganapathy.c@gmail.com>
2014-11-03 20:50:44 -0800

    Builder pattern, testing completed

--- src/pattern/Polynomial.java
+++ src/pattern/Polynomial.java
@@ -1,36 +1,86 @@
 package pattern;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Polynomial {
 	
 	private Map<Integer,Float> termMap;
 	
-	private Polynomial(){
+	public void print(){
+		
+		System.out.println("Polynomial ==");
+		
+		StringBuilder output = new StringBuilder();
+		for(Integer key: termMap.keySet())
+			output.append(getTermString(key,termMap.get(key))+" + ");
+		
+		System.out.println(output.toString());
 		
 	}
 	
-	public PolynomialBuilder getBuilder(){
-		return null;
+	private String getTermString(Integer exp, Float coeff) {
+		// TODO Auto-generated method stub
+		return ""+coeff+"*x^"+exp;
+	}
+
+	private Polynomial( Map<Integer,Float> termMap){
+		
+		this.termMap = termMap;
 		
 	}
+	
+
 	
 	public Polynomial add(Polynomial other){
-		return other;
+		
+		Builder pBuilder = new Builder();
+		
+		for(int exp:this.termMap.keySet()){
+			pBuilder.put(exp, termMap.get(exp));
+			
+		}
+		
+		for(int exp:other.termMap.keySet()){
+			pBuilder.put(exp, other.termMap.get(exp));
+			
+		}		
+		return pBuilder.build();
 		
 	}
 	
-	class PolynomialBuilder{
-		private Map<Integer,Float> builderMap = null;
+	private static Map<Integer,Float> mapCopy(Map<Integer,Float> input){
 		
-		public void put(int exp, float base){
+		Map<Integer,Float> output = new HashMap<Integer,Float>();
+		
+		for(Integer key:input.keySet())
+			output.put(key, input.get(key));
+		
+		return output;
+		
+	}
+	
+	public static class Builder{
+		private Map<Integer,Float> builderMap =  new HashMap<Integer,Float>();
+		
+		public Builder put(int exp, float base){
+			
+			Float oldBase = builderMap.get(exp);
+			if(oldBase==null)
+				builderMap.put(exp, base);
+			else
+				builderMap.put(exp, base+oldBase);
+			
+			return this;
 			
 		}
 		
 		public Polynomial build(){
-			return null;
+			return new Polynomial(mapCopy(builderMap));
 			
 		}
+		
+
 	}
 
 }
---
+++ src/pattern/PolynomialClient.java
@@ -0,0 +1,17 @@
+package pattern;
+
+public class PolynomialClient {
+
+	public static void main(String[] args) {
+		
+		Polynomial p = new Polynomial.Builder().put(1,2).put(2,2).build();
+		p.print();
+		
+		Polynomial p2 = new Polynomial.Builder().put(3,1).put(2,1).build();
+		
+		Polynomial p3 = p2.add(p);
+		p3.print();
+
+	}
+
+}
