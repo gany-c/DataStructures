3c3d2af901f6544a346ba607bf2e50b4282bdb5d
Gany Chidambaram <ganapathy.c@gmail.com>
2014-11-11 19:27:15 -0800

    Binary Search Tree tested and complete

--- src/tree/BinarySearchTree.java
+++ src/tree/BinarySearchTree.java
@@ -5,25 +5,71 @@ import base.TreeNode;
 
 public class BinarySearchTree {
 	
-	TreeNode root = null;
+	TreeNode header = new TreeNode();
+
+	
+	public BinarySearchTree(){
+		header.setValue(Integer.MIN_VALUE);
+		//A header node above the root is needed because the root itself 
+		// could be deleted
+		// makes things simpler
+	}
+	
+	
+	public void preOrder(){
+		System.out.println();
+		preOrder(this.header.getRight());
+	}
+	
+	 private  void preOrder(TreeNode input)
+	 {
+		if(input == null)
+			return;
+		else
+		{
+			System.out.print(input.getValue()+", ");
+			preOrder(input.getLeft());
+			preOrder(input.getRight());
+		}
+	 }	
 
 
 	public void insert(int value){
-		insertNode(root,value);
+		insertNode(header,value);
+		//Can start at the header because all nodes will be 
+		// greater and hence to its right
 	}
 	
 	public boolean search(int value){
 		
-		return false;
+		//can start searching from the right of the header
+		if(this.header.getRight()==null)		
+			return false;
+		else
+			return search(this.header.getRight(),value);
 	}
 	
+	private boolean search(TreeNode input, int value) {
+
+		if(input.getValue()==value)
+			return true;
+		else if(input.getValue() < value)
+		{
+			if(input.getRight() == null)
+				return false;
+			else return search(input.getRight(), value);
+		}
+		else
+		{
+			if(input.getLeft() == null)
+				return true;
+			else return search(input.getLeft(),value);
+		}
+	}
+
 	private void insertNode(TreeNode input,int value)
 	{
-		if(input == null)
-		{
-			this.root = createNode(value);
-		}		
-		else if(value<input.getValue())
+		if(value<input.getValue())
 		{
 			if(input.getLeft()==null)
 				input.setLeft(createNode(value));
@@ -52,12 +98,22 @@ public class BinarySearchTree {
 	
 	public  void delete(int target)
 	{
-		TreeNode parent = locateParent(root,target);
+		if(this.header.getRight() == null)
+			return;
+
+		System.out.println("deleting "+target);
+		
+		
+		//The header itself could be the parent here
+		//so we start from the header, not header.right()
+		TreeNode parent = locateParent(this.header,target);
+		if(parent == null)
+			return;
 		
 		boolean leftSide = true;
 		TreeNode targetNode = null;
 		
-		if(parent.getLeft().getValue()==target)
+		if(parent.getLeft()!=null&&parent.getLeft().getValue()==target)
 		{
 			leftSide = true;
 			targetNode = parent.getLeft();
@@ -100,6 +156,8 @@ public class BinarySearchTree {
 		
 	}
 
+
+
 	private static void setDesc(TreeNode parent, boolean leftSide,
 			TreeNode desc) {
 		
@@ -110,11 +168,12 @@ public class BinarySearchTree {
 		
 	}
 
-	private  TreeNode locateParent(TreeNode input,int target) {
+	private TreeNode locateParent(TreeNode input,int target) {
 		
 		if(input==null)
 			return null;
-		else if(input.getLeft().getValue()==target||input.getRight().getValue()==target)
+		else if((input.getLeft()!=null&&input.getLeft().getValue()==target)||
+				(input.getRight()!=null&&input.getRight().getValue()==target))
 			return input;		
 		else 
 		{
@@ -137,4 +196,44 @@ public class BinarySearchTree {
 		} 
 		return node;
 	} 
+	
+	public static void main(String[] args){
+		
+		BinarySearchTree bst = new BinarySearchTree();
+		bst.insert(50);
+		bst.insert(3);
+		bst.insert(100);
+		bst.insert(11);
+		
+		
+		bst.preOrder();
+		
+		bst.delete(50);
+		
+		System.out.println();
+		
+		bst.preOrder();
+		
+		bst.delete(3);
+		System.out.println();
+		
+		bst.preOrder();
+		
+		System.out.println();
+		System.out.println("Is 100 present = "+bst.search(100));
+		
+		
+		System.out.println("Is 101 present = "+bst.search(101));
+		
+		bst.insert(11);
+		bst.preOrder();
+		
+		bst.insert(7);
+		bst.preOrder();
+		
+		bst.insert(105);
+		bst.preOrder();
+		
+		
+	}
 }
