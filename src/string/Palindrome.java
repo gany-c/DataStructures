f5123cdb30ab3e01567b0f1e2b72e2058f462ff6
Gany Chidambaram <ganapathy.c@gmail.com>
2014-11-23 14:34:56 -0800

    Completed K-palindrome

--- src/string/Palindrome.java
+++ src/string/Palindrome.java
@@ -2,6 +2,12 @@ package string;
 
 public class Palindrome {
 	
+	/**
+	 * http://www.careercup.com/question?id=6287528252407808
+	 *
+	 *A k-palindrome is a string which transforms into a palindrome on removing at most k characters. 
+	 */
+	
 	public boolean isPalindrome(String input){
 		
 		
@@ -32,6 +38,39 @@ public class Palindrome {
 	
 		
 	}
+	
+	
+	public boolean isKPalindrome(String input, int k){
+		
+		if(input == null || k < 0)
+			return false;
+		else if(input.length() == 1)
+			return true;
+		else if(input.length() ==2)
+		{
+			if(input.charAt(0) == input.charAt(1))
+				return true;
+			else if(k > 0)
+				return true;
+			else 
+				return false;
+		}
+		else
+		{
+			if(input.charAt(0)==input.charAt(input.length()-1))
+				return isKPalindrome(input.substring(1,input.length()-1),k);
+			else
+			{
+				if(k<=0)
+					return false;
+				else
+					return isKPalindrome(input.substring(0,input.length()-1),k-1) ||
+							isKPalindrome(input.substring(1,input.length()),k-1);
+			}
+		}
+		
+		
+	}
 
 	public static void main(String[] args) {
 	
@@ -40,6 +79,16 @@ public class Palindrome {
 		System.out.println(p.isPalindrome("tamil"));
 		
 		System.out.println(p.isPalindrome("able was I ere I saw elba"));
+		
+		System.out.println("is K palindrome = "+p.isKPalindrome("malayalam",0));
+		System.out.println("is K palindrome = "+p.isKPalindrome("malayalam",1));
+		System.out.println("is K palindrome = "+p.isKPalindrome("valayalam",1));
+		System.out.println("is K palindrome = "+p.isKPalindrome("valayalam",2));
+		System.out.println("is K palindrome = "+p.isKPalindrome("emalayalam",1));
+		System.out.println("is K palindrome = "+p.isKPalindrome("maeelayalam",1));
+		System.out.println("is K palindrome = "+p.isKPalindrome("maeelayalam",2));
+		System.out.println("is K palindrome = "+p.isKPalindrome("some",3));
+		System.out.println("is K palindrome = "+p.isKPalindrome("some",2));
 		
 	}
 
