d291cd6d4fe484e1745c89cedaf6a8468eed2de4
Gany Chidambaram <ganapathy.c@gmail.com>
2015-03-08 23:36:01 -0700

    This bloom filter works :)

--- src/bloomfilter/BloomFilter.java
+++ src/bloomfilter/BloomFilter.java
@@ -4,102 +4,42 @@ import java.util.Arrays;
 
 public class BloomFilter {
 	
-	private int[] store = new int[64];
+	private int[] store = new int[32];
 	
-	private int[] getBinaryArray(int input){
+
+
+	private int hashFunction(int xint, int i, int m){
 		
-		String binString = Integer.toBinaryString(input);
+		long x = xint;
 		
+		long result = ((x*x +x*x*x)*i) % m;
+		System.out.println("result = "+result);
 		
-		int[] output = new int[64];
-		
-		for(int i= binString.length()-1;i>=0;i--)
-		{
-			
-			output[i] = Integer.parseInt(""+binString.charAt(i));
-		}
-		
-		return output;
-	}
+		return (int)result;
+	}	
 	
-/*	private int hashFunction(int x, int i, int m){
-		
-		long a = x*x;
-		
-		System.out.println("a = "+a);
-		
-		long b = x*a;
-		
-		System.out.println("b = "+b);
-		
-		long c = a + b;
-		
-		System.out.println("c = "+c);
-		
-		long product = c * i;
-		
-		System.out.println("product = "+product);
-		
-		long out = product % m;
-		
-		System.out.println("out = "+out);
-				
-		
-		return  (int)out;
-	}*/
-	
-	
-	private int hashFunction(int x, int i, int m){
+	private int hashFunction2(int x, int i, int m){
 		
 		
 		return (int)(Math.pow(x, i)) % m;
 	}
-	
-	
-	
-	private int hashChain(int x, int m){
-		
-		int step1 = hashFunction(x,1,m);
-		
-		System.out.println("step1 = "+step1);
-		
-		int step2 = hashFunction(step1,2,m);
-		
-		System.out.println("step2 = "+step2);
-		
-		
-		int step3 = hashFunction(step2,3,m);
-		
-		System.out.println("step3 = "+step3);
-		
-		return step3;
-		
-	}
-	
-	private void xor(int[] input){
-		
-		boolean alreadyPresent = true;
-		
-		for(int i=0;i<input.length;i++){
-			
-			if(input[i]==1 && store[i]==1)
-				continue;
-			else if(input[i]==1) 
-			{
-				alreadyPresent = false;
-				store[i] =1;
-			}
-		}
-		
-		System.out.println("alreadyPresent = "+alreadyPresent);
-	}
+
 	
 	public void storeVal(int input){
 		
-		int hashedVal = hashChain(input,64);
+		int bit1 = hashFunction(input, 1, 32);
+		int bit2 = hashFunction(input,2, 32);
+		int bit3 = hashFunction(input,3,32);
 		
-		int[] hashedArray = getBinaryArray(hashedVal);
-		xor(hashedArray);
+		if(this.store[bit1] == 1 && this.store[bit2] == 1 && this.store[bit3] == 1)
+		{
+			System.out.println(input + "already present");
+		}
+		
+		this.store[bit1] = 1;
+		this.store[bit2] = 1;
+		this.store[bit3] = 1;
+		
 		System.out.println("-----------------------------------");
 		
 	}
@@ -109,11 +49,14 @@ public class BloomFilter {
 		
 		BloomFilter bf = new BloomFilter();
 		
-		bf.storeVal(1975);	
-		bf.storeVal(1985);
-		bf.storeVal(1995);
-		bf.storeVal(2005);
-		bf.storeVal(2015);
+		bf.storeVal(2013);	
+		bf.storeVal(2010);
+		bf.storeVal(2007);
+		//2013, 2010, 2007, 2004, 2001, 1998
+		bf.storeVal(2004);
+//		bf.storeVal(2001);
+//		bf.storeVal(1998);
+
 	
 		for(int i=0;i<bf.store.length;i++){
 			System.out.println(i+". "+bf.store[i]);
