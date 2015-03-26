1decbc520516cb7d47c06fcd78b846ba01d1e4ec
Gany Chidambaram <ganapathy.c@gmail.com>
2015-03-25 22:14:33 -0700

    All permutations of a String

--- src/string/StringUtil.java
+++ src/string/StringUtil.java
@@ -1,5 +1,8 @@
 package string;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class StringUtil {
 	
 	public int indexOf(String main, String sub){
@@ -36,13 +39,100 @@ public class StringUtil {
 		}
 	}
 	
+	private Set<String> findAllPerm(String input){
+		
+		if(input==null||input.trim().isEmpty())
+			return null;
+		
+		Set<String> output = new HashSet<String>();
+		
+		if(input.length() == 1)
+		{
+			output.add(input);
+			return output;
+		}
+		else
+		{
+			for(int i =0;i<input.length();i++)
+			{
+				char c = input.charAt(i);
+				
+				String sub = null;
+				
+				if(i ==0)
+				{
+					sub = input.substring(1);
+				}
+				else if(i == input.length()-1)
+				{
+					sub = input.substring(0,input.length()-1);
+				}
+				else
+				{
+					sub = input.substring(0,i) + input.substring(i+1,input.length());
+				}
+				
+				Set<String> subSet = findAllPerm(sub);
+				
+				for(String subOut:subSet)
+				{
+					output.add(""+c+subOut);
+				}
+				
+				
+			}
+			
+			return output;
+		}
+		
+		
+	}
+	
 	public static void main(String[] args){
 		
 		StringUtil util = new StringUtil();
 		
-		System.out.println(util.indexOf("Vatapi Ganapathy","Gana"));
+/*		System.out.println(util.indexOf("Vatapi Ganapathy","Gana"));
 		System.out.println(util.indexOf("Vatapi Ganapathy","Vatapi"));
-		System.out.println(util.indexOf("Vatapi Ganapathy","arch"));
+		System.out.println(util.indexOf("Vatapi Ganapathy","arch"));*/
+		
+		Set<String> output = util.findAllPerm("a");
+		
+		for(String perm:output){
+			System.out.println(perm);
+		}
+		
+		System.out.println("--------------------------");
+		
+		output = util.findAllPerm("ab");
+		
+		for(String perm:output){
+			System.out.println(perm);
+		}
+		
+		System.out.println("--------------------------");
+		
+		output = util.findAllPerm("aa");
+		
+		for(String perm:output){
+			System.out.println(perm);
+		}
+		
+		System.out.println("--------------------------");
+		
+		output = util.findAllPerm("abc");
+		
+		for(String perm:output){
+			System.out.println(perm);
+		}
+		
+		System.out.println("--------------------------");
+		
+		 output = util.findAllPerm("Vatapi");
+		
+		for(String perm:output){
+			System.out.println(perm);
+		}
 		
 	}
 
