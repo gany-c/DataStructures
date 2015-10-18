e2a5498cf0e820761f71e5a4c88d081b9613c41e
Ganapathy Chidambaram <gchidambaram@walmart.com>
2015-10-18 12:08:24 -0700

    comments and new test case

--- src/fb/StockUtil.java
+++ src/fb/StockUtil.java
@@ -6,15 +6,32 @@ import java.util.Arrays;
 /**
  * this is just a period's data
 you have to find out the best days to buy and sell in retrospect, but do it linear time
+
  * @author Ramanan
+ * 
+ * one n^2 approach is to compare all differences
+ * it can be improved with a recursive approach but that would still be n^2
  *
  */
 public class StockUtil {
 	
+	/**
+	 * 
+	 * @author gchidam
+	 * 
+	 * Keep track of 4 states as you traverse the list
+	 *
+	 */
 	private enum State{
 		STARTED,MIN_FOUND,MAX_FOUND,CANDIDATE_MIN_FOUND;
 	}
 	
+	/**
+	 * 
+	 * @author gchidam
+	 *
+	 * create a tuple class to store the index and price
+	 */
 	private static class Tuple{
 		int price = -1;
 		int index = -1;
@@ -26,7 +43,19 @@ public class StockUtil {
 		
 	}
 	
-	//actually ends up finding the best prices, not best days i.e. not the indexes
+	/**
+	 *  1. when you start take the first value as the minimum value.
+	 *  2. if you are descending a slope that is finding values that are
+	 *     lower than the minimum, keep resetting the minimum.
+	 *  3. If a value greater than the minimum is found, then mark it as maximum.
+	 *  4. For the remainder of the loop if, greater maxima are found, reset the maximum
+	 *  5. Only other thing which needs to be marked is a lower minimum being found, 
+	 *     5.1. Mark it as candidate minimum
+	 *     5.2. Candidate minimum will be reset as a minimum if a maximum with greater difference can be found.
+	 *     5.3. Candidate minimum will be reset if lower mimima can be found.
+	 * @param prices
+	 * @return
+	 */
 	public Tuple[] findBestDays(int[] prices){
 		
 		if(prices==null||prices.length<=1)
@@ -126,6 +155,10 @@ public class StockUtil {
 		System.out.println(Arrays.toString(output));	
 		
 		output = su.findBestDays(new int[]{1,10,9,0,5,11});
+		
+		System.out.println(Arrays.toString(output));	
+		
+		output = su.findBestDays(new int[]{2,10,9,1,0,5,11});
 		
 		System.out.println(Arrays.toString(output));		
 
