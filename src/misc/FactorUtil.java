b19c9255b1392225768be6f0ab7b68041965fd88
Gany Chidambaram <ganapathy.c@gmail.com>
2015-12-06 22:13:26 -0800

    comments

--- src/misc/FactorUtil.java
+++ src/misc/FactorUtil.java
@@ -5,12 +5,25 @@ import java.util.TreeSet;
 
 public class FactorUtil {
 	
+	/**
+	 * 
+	 * @param input
+	 * @return
+	 * 
+	 * 1. if input is 0 or lesser return null
+	 * 2. if input is 1, return a set containing 1.
+	 * 3. i Starting from 1 and until input/2
+	 * 3.a. check if input is divisible by i
+	 * 3.b. if yes, add divisor and quotient to the set
+	 * 4. Use a tree set to print in order
+	 */
 	public Set<Integer> findAllFactors(int input){
 		
+		// if input is 0 or lesser return null
 		if(input <= 0)
 			return null;
 		else if(input == 1)
-		{
+		{//if input is 1, return a set containing 1.
 			Set<Integer> out = new TreeSet<Integer>();
 			out.add(1);
 			return out;
@@ -19,10 +32,13 @@ public class FactorUtil {
 		{
 			Set<Integer> out = new TreeSet<Integer>();
 			
+			//Starting from 1 and until input/2
 			for(int i =1;i <= input/2;i++)
 			{
+				//check if input is divisible by i
 				if(input%i == 0)
 				{
+					//if yes add divisor and quotient to the set
 					out.add(i);
 					out.add(input/i);
 				}
@@ -36,10 +52,23 @@ public class FactorUtil {
 		
 	}
 	
+	/**
+	 * 
+	 * @param input
+	 * @return
+	 * 
+	 * 1. if input equals 0, 1 or 2 return special cases.
+	 * 2. create empty output set
+	 * 3. for i from 2 to the number
+	 * 3.a. if output contains i, skip the loop
+	 * 3.b. if i perfectly divides the input
+	 * 3.c. add i to the output, recursively invoke it on the quotient
+	 * 
+	 */
 	public Set<Integer> findPrimeFactors(int input){
 		
 		//DEBUG: System.out.println("input = "+input);
-		
+		//if input equals 0, 1 or 2 return special cases.
 		if(input <= 0)
 			return null;
 		else if(input == 1)
@@ -57,17 +86,23 @@ public class FactorUtil {
 		}
 		else
 		{
+			//create empty output set
 			Set<Integer> out = new TreeSet<Integer>();
 			
-			
+			//for i from 2 to the number
 			for(int i =2; i <= input;i++)
 			{
+				//if output contains i, skip the loop
 				if(out.contains(i))
 					continue;
 				
+				//if i perfectly divides the input
 				if(input%i == 0)
 				{
+					//add i to the output
 					out.add(i);
+					
+					//recursively invoke it on the quotient
 					out.addAll(findPrimeFactors(input/i));
 					return out;
 				}
@@ -91,6 +126,7 @@ public class FactorUtil {
 		
 		System.out.println(u.findPrimeFactors(2));
 		System.out.println(u.findPrimeFactors(5));
+		System.out.println(u.findPrimeFactors(30));
 		System.out.println(u.findPrimeFactors(366));
 		System.out.println(u.findPrimeFactors(360));
 		System.out.println(u.findPrimeFactors(213));
