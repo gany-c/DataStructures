47b9b9c19d21273e98978294989c7f645c7865d6
Gany Chidambaram <ganapathy.c@gmail.com>
2015-12-10 00:43:11 -0800

    comments

--- src/misc/NumberUtil.java
+++ src/misc/NumberUtil.java
@@ -2,28 +2,55 @@ package misc;
 
 public class NumberUtil {
 	
+	/**
+	 * Find the square root of a number passed in as a double type, with a second parameter precision.
+	 * 
+	 * Solution: Do through binary search
+	 * 
+	 * 1. if the number is less than 0, throw an exception.
+	 * 2. if the number is the same as 0 or 1 return the same number.
+	 * 3. else begin a typical binary search, set start =0, end = number
+	 * 4. do while end -start is greater than precision.
+	 * 4.1. get average.
+	 * 4.2. calculate the product of the average.
+	 * 4.3. if the product equals the number, return the average.
+	 * 4.4. if you fall lesser than the number, set start to mid
+	 * 4.5. else set end to mid
+	 * @param number
+	 * @param precision
+	 * @return
+	 */
 	public double sqrt(double number, double precision){
 		
+		//if the number is less than 0, throw an exception.
 		if(number <0)
 			throw new IllegalArgumentException("Give a value greater than 0:"+number);
-		
+		//if the number is the same as 0 or 1 return the same number.
 		if(number==0 ||number ==1)
 			return number;
 		else
 		{
+			//else begin a typical binary search
+			//set start =0, end = number
 			double start =0, mid =0, end = number;
 			double midSq =0;
 			
+			//do while end -start is greater than precision.
 			while (end - start> precision)
 			{
+				// get average.
 				mid = (start+end)/2;
 				
+				//calculate the product of the average.
 				midSq = mid * mid;
 				
+				//if the product equals the number, return the average.
 				if(midSq == number)
-					return midSq;
+					return mid;
+				//if you fall lesser than the number, set start to mid
 				else if(midSq < number)
 					start = mid;
+				//else set end to mid
 				else
 					end = mid;
 			
