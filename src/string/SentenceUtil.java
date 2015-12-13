20f11601b63b49a53309dc26d9f2c3ce688a9284
Ganapathy Chidambaram <gchidambaram@walmart.com>
2015-12-13 14:25:53 -0800

    Comments

--- src/string/SentenceUtil.java
+++ src/string/SentenceUtil.java
@@ -3,14 +3,30 @@ package string;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * 
+ * @author gchidam
+ * 
+ * PROBLEM: Given a Set of strings as a dictionary and an input String, check if the sentence is composed of words in the string.
+ *
+ * 1. if the sentence is null or empty or if the dictionary is null return false
+ * 2. start from the first character in the input, and keep building larger substrings from the start
+ * 2.1. if the dictionary doesn't contain the substring, continue i.e. build a larget prefix
+ * 2.2. if there is a match with a dictionary word, 
+ * 2.2.1. if you have reached the end of the input, return true
+ * 2.2.2. Otherwise make a recursive for the rest of the string and the dictionary.
+ */
 public class SentenceUtil {
 	
 	public boolean isValidSentence(String sentence, Set<String> dictionary){
 		
+		//if the sentence is null or empty or if the dictionary is null return false
 		if(sentence == null || sentence.trim().isEmpty() || dictionary==null)
 			return false;
 		else
 		{
+			//start from the first character in the input
+			// and keep building larger substrings from the start
 			for(int i =1;i <=sentence.length();i++)
 			{
 				/**
@@ -20,14 +36,17 @@ public class SentenceUtil {
 				 * endIndex = first char not in substring i.e. end +1
 				 */
 				String prefix  = sentence.substring(0, i);
-				//System.out.println("prefix = "+prefix);
 				
+				
+				// if the dictionary doesn't contain the substring, continue
 				if(!dictionary.contains(prefix))
 					continue;
 				else
 				{
+					//else, if you've reached the end of input return true
 					if(i == sentence.length())
 						return true;
+					//else, make a recursive call from the next point to the rest of input
 					else if(isValidSentence(sentence.substring(i),dictionary))
 						return true;
 				}
