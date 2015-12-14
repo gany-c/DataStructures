c3ef47e4daf7148635f54b7b931a03b55cbc5a74
Gany Chidambaram <ganapathy.c@gmail.com>
2015-12-13 19:49:48 -0800

    Serialize a tree of characters with minimum space usage, i.e. as bits

---
+++ src/tree/CharNode.java
@@ -0,0 +1,36 @@
+package tree;
+
+
+public class CharNode{
+
+private char value;
+private CharNode left;
+private CharNode right;
+
+public char getValue(){
+return value;
+}
+
+public void setValue(char value){
+this.value = value;
+}
+
+public CharNode getLeft(){
+return left;
+}
+
+public CharNode getRight(){
+return right;
+}
+
+public void setLeft(CharNode node){
+this.left = node;
+}
+
+public void setRight(CharNode node){
+this.right = node;
+}
+}
+
+
+
---
+++ src/tree/SerializeTree.java
@@ -0,0 +1,75 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SerializeTree {
+	
+	private String convertToString(char a){
+		
+		int index = Character.getNumericValue(a) - 10;
+		String binString = Integer.toBinaryString(index);
+		
+		while(binString.length() < 5){
+			
+			binString = "0"+binString;
+		}
+		
+		return binString;
+	}
+	
+	private byte serializeNode(CharNode c){
+		
+		StringBuffer out = new StringBuffer();
+		
+		if(c.getLeft()==null)
+			out.append("0");
+		else
+			out.append("1");
+		
+		if(c.getRight()==null)
+			out.append("0");
+		else
+			out.append("1");
+		
+		out.append(convertToString(c.getValue()));
+		
+		return Byte.parseByte(out.toString(), 2);
+		
+		
+	}
+	
+	public List<Byte> serializeTree(CharNode head){
+		
+		if(head == null)
+			return null;
+		
+		List<Byte> output = new ArrayList<Byte>();
+		
+		serializeTree(head,output);
+		
+		return output;
+	}
+
+	private void serializeTree(CharNode head, List<Byte> output) {
+		
+		output.add(serializeNode(head));
+		
+		if(head.getLeft()!=null)
+			serializeTree(head.getLeft(),output);
+		
+		if(head.getRight()!=null)
+			serializeTree(head.getRight(),output);
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SerializeTree st = new SerializeTree();
+		
+		System.out.println(st.convertToString('a'));
+		System.out.println(st.convertToString('b'));
+		System.out.println(st.convertToString('z'));
+	}
+
+}
