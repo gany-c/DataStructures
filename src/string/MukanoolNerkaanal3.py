9f854d617ea69a70dd18125e75e1d0658204c836
Gany Chidambaram <ganapathy.c@gmail.com>
2021-08-09 15:42:10 -0700

    Create MukanoolNerkaanal3.py

---
+++ src/string/MukanoolNerkaanal3.py
@@ -0,0 +1,3 @@
+Given the current directory and a target path i.e. cwd and cd "...", get the full target path.
+
+The cd path can start with '/', have mulitiple dirs separated by '/' and . and .. operators
